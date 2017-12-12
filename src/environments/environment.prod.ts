ed881456090573b50dc23f464f2a32be97d123f5
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2017-12-12 22:34:31 +0100

    Changed back to develop

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  api: {
+    url: 'https://zvdo74-test-api.herokuapp.com/api/v1',
+  }
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,11 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
-  production: true,
+  production: false,
   api: {
-    url: 'https://zvdo74-test-api.herokuapp.com/api/v1',
+    url: 'http://localhost:8080/api/v1',
   }
 };
